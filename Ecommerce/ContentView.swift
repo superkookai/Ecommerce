402683d72c9692670497047968184d532694eea2
superkookai <weerawut@live.com>
2025-04-08 09:38:49 +0700

    Initial Commit

---
+++ Ecommerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ecommerce
+//
+//  Created by Weerawut Chaiyasomboon on 08/04/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Weerawut Chaiyasomboon on 08/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
